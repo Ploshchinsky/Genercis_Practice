2401f472d9c555be07d1e05c07bf35835359cefc
Anton P. <made1of1fire@gmail.com>
2024-05-08 11:35:51 +0500

    feat(Participant): added general abstract class Participant

---
+++ src/ParticipantsAndTeams/Participant.java
@@ -0,0 +1,14 @@
+package ParticipantsAndTeams;
+
+public abstract class Participant {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+}
