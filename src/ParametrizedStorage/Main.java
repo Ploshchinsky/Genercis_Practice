91242f9d9194baf0f7927d907da10a91548e6ed2
Anton P. <made1of1fire@gmail.com>
2024-05-08 11:19:02 +0500

    feat(test): added functional tests in method Main

--- src/ParametrizedStorage/Main.java
+++ src/ParametrizedStorage/Main.java
@@ -2,6 +2,27 @@ package ParametrizedStorage;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book book1 = new Book("Martin Eden", "J.London", 1909);
+        Book book2 = new Book("Thanatonauts", "Bernar Werber", 1994);
+        Book book3 = new Book("Goethe", " Johann Wolfgang von Goethe", 1804);
+
+        //Check create functional
+        BookStorage bookStorage = new BookStorage();
+        bookStorage.save(book1);
+        bookStorage.save(book2);
+        bookStorage.save(book3);
+        System.out.println(bookStorage.getList() + "\n");
+
+        //Check read functional
+        System.out.println(bookStorage.getById(2));
+
+        //Check delete functional
+        bookStorage.deleteById(1);
+        System.out.println(bookStorage.getList() + "\n");
+
+        //Check update functional
+        book3.setYear(1808);
+        bookStorage.updateById(1, book3);
+        System.out.println(bookStorage.getList() + "\n");
     }
 }
