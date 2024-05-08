e40ef56e3821f4744034cdc8e6ec72450f9f5722
Anton P. <made1of1fire@gmail.com>
2024-05-08 11:46:28 +0500

    fix(Worker): missprint age type String -> int

--- src/ParticipantsAndTeams/Worker.java
+++ src/ParticipantsAndTeams/Worker.java
@@ -2,7 +2,7 @@ package ParticipantsAndTeams;
 
 public class Worker
         extends Participant {
-    public Worker(String name, String age) {
+    public Worker(String name, int age) {
         super(name, age);
     }
 }
