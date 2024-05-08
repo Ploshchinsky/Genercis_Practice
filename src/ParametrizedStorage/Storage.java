59e827fed828ffec6583b8cfa5a8075cd672ed1e
Anton P. <made1of1fire@gmail.com>
2024-05-08 11:04:22 +0500

    fix(Storage): fixed method update to updateById

--- src/ParametrizedStorage/Storage.java
+++ src/ParametrizedStorage/Storage.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface Storage<T> {
     public boolean save(T element);
     public T getById(int id);
-    public void update(T updatedElement);
+    public void updateById(int id,T updatedElement);
     public void deleteById(int id);
     public List<T> getList();
 }
