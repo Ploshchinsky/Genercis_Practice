3aac2c63899b5b04e642f71c823796566a6ab482
Anton P. <made1of1fire@gmail.com>
2024-05-29 15:03:31 +0500

    feat(Wildcards2): Pair.class -> which stores a pair of elements (Key, Value), and methods to handle limited wildcards

---
+++ src/Wildcards2/Main.java
@@ -0,0 +1,18 @@
+package Wildcards2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Pair<String, Integer> pair1 = new Pair<>("First", 1);
+        Pair<String, Integer> pair2 = new Pair<>("Second", 2);
+        Pair<String, Integer> pair3 = new Pair<>("Third", 3);
+        pair2.setValue(22);
+        String pair3key = pair3.getKey();
+        pair1.setKey(pair3key + pair1.getKey());
+
+        List<Pair<String, Integer>> pairList = List.of(pair1, pair2, pair3);
+        pairList.forEach(System.out::println);
+    }
+}
